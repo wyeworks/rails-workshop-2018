8de10528c68c2d7c39538e7945452b9cdfa8aa4c
Nicolas Ferraro <nferraro1996@gmail.com>
2018-03-17 14:51:46 -0300

    Add List create, show

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,32 @@
+class ListsController < ApplicationController
+  before_action :set_list, only: [:show]
+  before_action :set_board, only: [:create]
+
+  def create
+    list = @board.lists.new(name: list_params[:name])
+
+    if list.save
+      render json: list, status: :created
+    else
+      render json: list.errors, status: :bad_request
+    end
+  end
+
+  def show
+    render json: @list, status: :ok
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def set_board
+    @board = Board.find(params[:board_id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :boards, only: %i[create show]
+  resources :boards, only: %i[create show] do
+    resources :lists, only: %i[create show]
+  end
 end
